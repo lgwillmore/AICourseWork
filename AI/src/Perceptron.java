dd2de8c1b7d2e7e064d26586427b1e7fcff94e58
Laurence <Laurence@DeskTop-windows>
2012-02-29 21:48:44 +0000

    Finished V1 of perceptron. Need to test it on some training and test data.

--- AI/src/Perceptron.java
+++ AI/src/Perceptron.java
@@ -1,48 +1,95 @@
-import java.util.ArrayList;
 
-
+/**
+ * This is a perceptron that is designed to take any amount of double input
+ * values, and return any amount of binary integer values you want.
+ * 
+ * @author Laurence
+ * 
+ */
 public class Perceptron {
 	Node[] nodes;
-	
-	public Perceptron(int input, int output){
-		nodes= new Node[output];
+
+	/**
+	 * Constructor
+	 * 
+	 * @param input
+	 *            = number of inputs. inputs will be doubles
+	 * @param output
+	 *            = number of outputs. binary int.
+	 */
+	public Perceptron(int input, int output) {
+		nodes = new Node[output];
 		for (int i = 0; i < nodes.length; i++) {
-			nodes[i]=new Node(input);
+			nodes[i] = new Node(input);
 		}
 	}
-	
-	public void train(ArrayList<Double> input, ArrayList<Integer> output){
-		
+
+	/*
+	 * Train nodes on input instance and expected results.
+	 */
+	public void train(double[] input, int[] expected) {
+		for (int i = 0; i < nodes.length; i++) {
+			nodes[i].train(input, expected[i]);
+		}
 	}
-	
-	private class Node{
-		double bias=1;
-		double initWeight=0.0;
+
+	/*
+	 * Get a prediction on the input
+	 */
+	public int[] predict(double[] input) {
+		int[] result = new int[nodes.length];
+		for (int i = 0; i < nodes.length; i++) {
+			result[i] = nodes[i].predict(input);
+		}
+		return result;
+	}
+
+	/**
+	 * Node is needed for each binary output you want. Every input goes to every
+	 * Node.
+	 * 
+	 * @author Laurence
+	 * 
+	 */
+	private class Node {
+		double bias = 1;
+		double initWeight = 0.0;
 		double[] weights;
-		
-		private Node(int inputs){
-			weights=new double[inputs+1];
-			//add weight for bias at index 0;
-			weights[0]=0.3;
+
+		private Node(int inputs) {
+			weights = new double[inputs + 1];
+			// add weight for bias at index 0;
+			weights[0] = 0.3;
 			for (int i = 1; i < weights.length; i++) {
-				weights[i]=initWeight;
+				weights[i] = initWeight;
 			}
 		}
-		
-		public void train(ArrayList<Double> in, int result){
-			
-		}
-		
-		public int predict(ArrayList<Double> in){
-			double sum=weights[0]*bias;
-			for(int i=0;i<in.size();i++){
-				sum+=weights[i+1]*in.get(i);
+
+		public void train(double[] in, int expected) {
+			if (predict(in) != expected) {
+				if (expected == 1) {
+					weights[0] += 1;
+					for (int i = 1; i < weights.length; i++) {
+						weights[i] += in[i];
+					}
+				} else {
+					weights[0] += 1;
+					for (int i = 1; i < weights.length; i++) {
+						weights[i] -= in[i];
+					}
+				}
 			}
-			if(sum>0)return 1;
+		}
+
+		public int predict(double[] in) {
+			double sum = weights[0] * bias;
+			for (int i = 0; i < in.length; i++) {
+				sum += weights[i + 1] * in[i];
+			}
+			if (sum > 0)
+				return 1;
 			return 0;
 		}
 	}
-	
-	
 
 }
